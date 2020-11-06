cbdfd772b36dec0ea16ed0c04cc8abe18204a3d2
Andrei_Viarenich <aviarenich@copyright.com>
2020-11-06 17:29:03 +0300

    Added Eureka service discovery

--- guest-service/src/main/java/com/cloud/sample/guestservice/GuestServiceApp.java
+++ guest-service/src/main/java/com/cloud/sample/guestservice/GuestServiceApp.java
@@ -2,8 +2,10 @@ package com.cloud.sample.guestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GuestServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(GuestServiceApp.class);
