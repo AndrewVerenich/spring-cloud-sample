14037b714f35afd11d3ad74888b67e653d82ca1c
Andrei_Viarenich <aviarenich@copyright.com>
2021-03-28 09:57:26 +0300

    Hystrix circuit breaker

--- api-gateway/src/main/java/com/cloud/sample/gateway/GatewayApp.java
+++ api-gateway/src/main/java/com/cloud/sample/gateway/GatewayApp.java
@@ -5,12 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@RestController
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class);
@@ -24,10 +29,16 @@ public class GatewayApp {
                         .and()
                         .path("/rooms/**")
                         .filters(f -> f
+                                .hystrix(h -> h.setFallbackUri("forward:/fallback"))
                                 .addResponseHeader("Hello", "World")
-                                .addRequestParameter("Param1","Value1")
+                                .addRequestParameter("Param1", "Value1")
                         )
                         .uri("lb://ROOMSERVICE:8083/"))
                 .build();
+    }
+
+    @GetMapping(path = "/fallback")
+    String fallbackMethod() {
+        return "Fallback method";
     }
 }
